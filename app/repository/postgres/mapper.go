c0902e6fa7f9a0b560c23197b55f008c2c5ea1d3
n.leontev <funkydrummaz@gmai.com>
2022-10-13 14:52:02 +0500

    mapper.go: added fields according to db entity "person"

--- app/repository/postgres/mapper.go
+++ app/repository/postgres/mapper.go
@@ -4,11 +4,15 @@ import "github.com/lozovoya/GolangUnitedSchool/app/domain"
 
 func DBPersonToPerson(p *DBPerson) *domain.Person {
 	return &domain.Person{
-		ID:        p.ID,
-		FirstName: StringPointerToString(p.FirstName),
-		LastName:  StringPointerToString(p.LastName),
-		SurName:   StringPointerToString(p.SurName),
-		Login:     StringPointerToString(p.Login),
+		ID:         p.ID,
+		FirstName:  StringPointerToString(p.FirstName),
+		LastName:   StringPointerToString(p.LastName),
+		Patronymic: StringPointerToString(p.Patronymic),
+		Login:      StringPointerToString(p.Login),
+		RoleId:     p.RoleId,
+		Passwd:     StringPointerToString(p.Passwd),
+		UpdatedAt:  p.UpdatedAt,
+		Deleted:    p.Deleted,
 	}
 }
 
