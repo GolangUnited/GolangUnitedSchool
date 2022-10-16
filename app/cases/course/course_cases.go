c76d1a4db4d9f7c4d115f5c8f5760c54f6afd978
Akzhol Kanibekuly <a.kanibekuly@ait-corp.net>
2022-10-16 15:45:43 +0600

    init

---
+++ app/cases/course/course_cases.go
@@ -0,0 +1,8 @@
+package course_cases
+
+type CourseCases struct {
+}
+
+func NewCourseCases() *CourseCases {
+	return &CourseCases{}
+}
--- app/domain/model.go
+++ app/domain/model.go
@@ -1,9 +1,19 @@
 package domain
 
+import "time"
+
 type Person struct {
 	ID        int64
 	FirstName string
 	LastName  string
 	SurName   string
 	Login     string
+}
+
+type Course struct {
+	ID        int64
+	Title     string
+	Status    string
+	StartDate time.Time
+	EndDate   time.Time
 }
