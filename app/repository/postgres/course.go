d7c6e7e65d55d506071be3b16bf72a76f4aa3456
Akzhol Kanibekuly <a.kanibekuly@ait-corp.net>
2022-11-01 06:47:51 +0600

    feat: course repo interface

--- app/repository/postgres/course.go
+++ app/repository/postgres/course.go
@@ -1,11 +1,14 @@
 package postgres
 
-import "context"
+import (
+	"context"
 
-func (r *PostgresRepository) CreateCourse(ctx context.Context) {
+	"github.com/lozovoya/GolangUnitedSchool/app/domain/model"
+)
 
-}
-
-func (r *PostgresRepository) GetCourseByID(ctx context.Context) {
-
-}
+func (r *PostgresRepository) CreateCourse(ctx context.Context, course *model.CourseCreate) (int64, error)
+func (r *PostgresRepository) GetCourseByID(context.Context, int64) (model.Course, error)
+func (r *PostgresRepository) GetCourses(context.Context) ([]model.Course, error)
+func (r *PostgresRepository) UpdateCourseByID(context.Context, int64, *model.CourseUpdate) error
+func (r *PostgresRepository) AddCourse(context.Context, *model.Course) (int64, error)
+func (r *PostgresRepository) DeleteCourseByID(context.Context, int64) error
--- app/repository/repository.go
+++ app/repository/repository.go
@@ -1,5 +1,17 @@
 package repository
 
+import (
+	"context"
+
+	"github.com/lozovoya/GolangUnitedSchool/app/domain/model"
+)
+
 // RepositoryInterface is main interface for all repo implementations
 type RepositoryInterface interface {
+	CreateCourse(context.Context, *model.CourseCreate) (int64, error)
+	GetCourseByID(context.Context, int64) (model.Course, error)
+	GetCourses(context.Context) ([]model.Course, error)
+	UpdateCourseByID(context.Context, int64, *model.CourseUpdate) error
+	AddCourse(context.Context, *model.Course) (int64, error)
+	DeleteCourseByID(context.Context, int64) error
 }
