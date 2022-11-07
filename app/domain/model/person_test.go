155a60dc4bcb768e98bb8ce1699a57aeaf101e62
passed3 <funkydrumma@yandex.ru>
2022-11-07 21:23:58 +0500

    started person val&tests

--- app/domain/model/person_test.go
+++ app/domain/model/person_test.go
@@ -2,8 +2,10 @@ package model
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 var testPerson Person
@@ -26,7 +28,7 @@ func TestPerson_ValidPerson(t *testing.T) {
 
 }
 
-func TestPerson_Name(t *testing.T) {
+func TestPerson_EmptyName(t *testing.T) {
 	emptyNameJson := []byte(`{
 	"first_name": "",
 	"last_name": "vasiliev",
@@ -42,7 +44,9 @@ func TestPerson_Name(t *testing.T) {
 	assert.Equal(t,
 		"[FirstName is a required field]",
 		testPerson.ValidatePerson().Error())
+}
 
+func TestPerson_ShortName(t *testing.T) {
 	shortNameJson := []byte(`{
 	"first_name": "v",
 	"last_name": "vasiliev",
@@ -58,6 +62,10 @@ func TestPerson_Name(t *testing.T) {
 	assert.Equal(t,
 		"[FirstName must be at least 2 characters in length]",
 		testPerson.ValidatePerson().Error())
+
+}
+
+func TestPerson_LongName(t *testing.T) {
 
 	longNameJson := []byte(`{
 	"first_name": "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
@@ -114,10 +122,13 @@ func TestPerson_longShortLastName(t *testing.T) {
 	assert.Equal(t,
 		"[LastName must be at least 2 characters in length]",
 		testPerson.ValidatePerson().Error())
+}
+
+func TestPerson_LongLastName(t *testing.T) {
 
 	LongLastNameJson := []byte(`{
 	"first_name": "vasil",
-	"last_name": "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
+	"last_name": "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
 	"patronymic": "vasiliveich",
 	"login": "12lkj2",
 	"role_id": 1,
@@ -131,4 +142,41 @@ func TestPerson_longShortLastName(t *testing.T) {
 		"[LastName must be a maximum of 50 characters in length]",
 		testPerson.ValidatePerson().Error())
 
+}
+
+func TestPerson_ValidatePerson(t *testing.T) {
+	type fields struct {
+		PersonId   int64
+		FirstName  string
+		LastName   string
+		Patronymic string
+		Login      string
+		RoleId     int
+		Passwd     string
+		UpdatedAt  time.Time
+		Deleted    bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &Person{
+				PersonId:   tt.fields.PersonId,
+				FirstName:  tt.fields.FirstName,
+				LastName:   tt.fields.LastName,
+				Patronymic: tt.fields.Patronymic,
+				Login:      tt.fields.Login,
+				RoleId:     tt.fields.RoleId,
+				Passwd:     tt.fields.Passwd,
+				UpdatedAt:  tt.fields.UpdatedAt,
+				Deleted:    tt.fields.Deleted,
+			}
+			tt.wantErr(t, n.ValidatePerson(), fmt.Sprintf("ValidatePerson()"))
+		})
+	}
 }
