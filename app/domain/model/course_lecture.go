d57715b455f2eb86d2c1cbf31237c7823a91255a
n.leontev <funkydrummaz@gmai.com>
2022-11-09 00:02:18 +0500

    course_lecture: validation+tests added

--- app/domain/model/course_lecture.go
+++ app/domain/model/course_lecture.go
@@ -1,6 +1,29 @@
 package model
 
+import (
+	"fmt"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
+)
+
 type CourseLecture struct {
-	CourseId  int64
-	LectureId int64
+	CourseId  int64 `json:"course_id" validate:"required,numeric"`
+	LectureId int64 `json:"lecture_id" validate:"required,numeric"`
+}
+
+func (n *CourseLecture) ValidateCourseLecture() error {
+	validate := validator.New()
+	english := en.New()
+	uni := ut.New(english, english)
+	trans, _ := uni.GetTranslator("en")
+	_ = enTranslations.RegisterDefaultTranslations(validate, trans)
+	err := validate.Struct(n)
+	errs := translateError(err, trans)
+	if errs != nil {
+		return fmt.Errorf("%s", errs)
+	}
+	return nil
+
 }
