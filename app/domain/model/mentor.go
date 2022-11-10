c6d3c77376feba526df494087557e9ee872197da
n.leontev <funkydrummaz@gmai.com>
2022-11-10 20:04:07 +0500

    mentor_note valid+tests

--- app/domain/model/mentor.go
+++ app/domain/model/mentor.go
@@ -1,5 +1,28 @@
 package model
 
+import (
+	"fmt"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
+)
+
 type Mentor struct {
-	MentorId int64 `json:"mentor_id" validate:"required,gt=0"`
+	MentorId int64 `json:"mentor_id" validate:"required,numeric,gt=0"`
+}
+
+func (n *Mentor) ValidateMentor() error {
+	validate := validator.New()
+	english := en.New()
+	uni := ut.New(english, english)
+	trans, _ := uni.GetTranslator("en")
+	_ = enTranslations.RegisterDefaultTranslations(validate, trans)
+	err := validate.Struct(n)
+	errs := translateError(err, trans)
+	if errs != nil {
+		return fmt.Errorf("%s", errs)
+	}
+	return nil
+
 }
