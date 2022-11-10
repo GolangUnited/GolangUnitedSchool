ab9144a6dee61107cf270da618cef5c3c4ac0d5d
n.leontev <funkydrummaz@gmai.com>
2022-11-11 00:18:45 +0500

    group contact with contacts valid regexp

--- app/domain/model/group_contact.go
+++ app/domain/model/group_contact.go
@@ -6,6 +6,7 @@ import (
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	enTranslations "github.com/go-playground/validator/v10/translations/en"
+	"regexp"
 )
 
 /*
@@ -35,6 +36,45 @@ func (n *GroupContact) ValidateGroupContact() error {
 	_ = enTranslations.RegisterDefaultTranslations(validate, trans)
 	err := validate.Struct(n)
 	errs := translateError(err, trans)
+	telegramContactType, _ := regexp.Compile("^@[a-zA-Z_0-9]+$")
+	telegramContactGroup, _ := regexp.Compile("")
+	discordContactType, _ := regexp.Compile("")
+	discordChannelContactType, _ := regexp.Compile("")
+	emailContactType, _ := regexp.Compile("")
+	phoneContactType, _ := regexp.Compile("")
+	vkProfileContactType, _ := regexp.Compile("")
+	switch n.ContactTypeId {
+	case 1:
+		if telegramContactValid := telegramContactType.MatchString(n.ContactValue); !telegramContactValid {
+			_ = fmt.Errorf("invalid telegram contact")
+		}
+	case 2:
+		if telegramContactGroup := telegramContactGroup.MatchString(n.ContactValue); !telegramContactGroup {
+			_ = fmt.Errorf("invalid telegram group contact")
+		}
+	case 3:
+		if discordContactType := discordContactType.MatchString(n.ContactValue); !discordContactType {
+			_ = fmt.Errorf("invalid discord contact")
+		}
+	case 4:
+		if discordChannelContactType := discordChannelContactType.MatchString(n.ContactValue); !discordChannelContactType {
+			_ = fmt.Errorf("invalid discord channel contact")
+		}
+	case 5:
+		if emailContactType := emailContactType.MatchString(n.ContactValue); !emailContactType {
+			_ = fmt.Errorf("invalid email contact")
+		}
+	case 6:
+		if phoneContactType := phoneContactType.MatchString(n.ContactValue); !phoneContactType {
+			_ = fmt.Errorf("invalid phone contact")
+		}
+	case 7:
+		if vkProfileContactType := vkProfileContactType.MatchString(n.ContactValue); !vkProfileContactType {
+			_ = fmt.Errorf("invalid vk profile")
+		}
+
+	}
+
 	if errs != nil {
 		return fmt.Errorf("%s", errs)
 	}
